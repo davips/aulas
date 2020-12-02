5b9547b89a1d31f23f7152458a585ceb5ac2b807
davips <davips@users.noreply.github.com>
2020-12-02 16:32:21 -0300

    Update __init__.py

--- aulas/__init__.py
+++ aulas/__init__.py
@@ -1,9 +1,4 @@
-from sklearn.tree import plot_tree as plot
-import matplotlib.pyplot as plt
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import StratifiedKFold
-from sklearn.preprocessing import OneHotEncoder as OHE
-from sklearn.preprocessing import LabelEncoder as LE
+# Importa coisas para abrir ARFF direto da URL.
 import pandas as pd
 import urllib.request
 from io import StringIO
@@ -13,8 +8,7 @@ import pandas as pd
 
 
 def df_remoto(url_do_arff, class_index=-1):
-    """Importa coisas para abrir ARFF direto da URL.
-
+    """
     Exemplo de uso: 
     X,y = Xy_remoto("https://raw.githubusercontent.com/lpfgarcia/ucipp/master/uci/hepatitis.arff")
     """
@@ -31,19 +25,23 @@ def df_remoto(url_do_arff, class_index=-1):
         return v
     return pd.DataFrame(data[0]).applymap(f)
 
-def nom_to_num(X, y):
-    """Binariza atributos nominais (cada atributo com n valores distintos vira n colunas)."""
-    ohe = OHE()
-    le = LE()
-    return ohe.fit_transform(X), le.fit_transform(y)
+#  Binariza atributos nominais (cada atributo com n valores distintos vira n colunas).
+from sklearn.preprocessing import OneHotEncoder as OHE
+# from sklearn.preprocessing import LabelEncoder as LE
+def nom_to_num(X):
+  ohe = OHE()
+  le = LE()
+  return ohe.fit_transform(X)  #, le.fit_transform(y)
 
+
+# Importa de arquivo local (ou do google drive)
 # LEMBRETE: scipy funciona em vez de liac-arff?
 def Xy_local(filename, class_index=-1):
-    """Importa de arquivo local (ou do google drive)
-    
+    """
     Exemplo de uso: 
     X,y = Xy_local("/home/usuario/dataset.arff")
     """
+    # !pip install liac-arff
     
     # Abre conjunto de dados.
     file = open(filename, "r")
@@ -56,32 +54,40 @@ def Xy_local(filename, class_index=-1):
     y = np.array(data[:, class_index])
     return X, y
 
+# Função para avaliar acurácia.
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+
 def acc_std(classifier, X, y):
-    """Função para avaliar acurácia."""
-    cv = StratifiedKFold(n_splits=10, shuffle=True, random_state=42)
-    accs = cross_val_score(classifier, X, y, cv=cv)
-    return round(np.mean(accs)*1000)/1000, round(np.std(accs)*1000)/1000
+  cv = StratifiedKFold(n_splits=10, shuffle=True, random_state=42)
+  accs = cross_val_score(classifier, X, y, cv=cv)
+  return round(np.mean(accs)*1000)/1000, round(np.std(accs)*1000)/1000
+
+
+# Substitui valores ausentes pela moda.
 
 def imputa_moda(df):
-    """Substitui valores ausentes pela moda."""
-    # Tira a cópia do dataframe, pois vai alterá-lo.
-    df_mode = df.copy()
+  # Tira a cópia do dataframe, pois vai alterá-lo.
+  df_mode = df.copy()
 
-    for column in df_mode.columns:
-        df_mode[column].fillna(df_mode[column].mode()[0], inplace=True)
-    return df_mode
+  for column in df_mode.columns:
+      df_mode[column].fillna(df_mode[column].mode()[0], inplace=True)
+  return df_mode
 
+# Converte dataframe do pandas em matrizes numpy para uso no sklearn.
 def df_to_Xy(df):
-    """Converte dataframe do pandas em matrizes numpy para uso no sklearn."""
-    X = df.to_numpy()[:, :-1]
-    y = df.to_numpy()[:, -1]
-    return X, y
+  X = df.to_numpy()[:, :-1]
+  y = df.to_numpy()[:, -1]
+  return X, y
 
+
+# Plota árvore
+from sklearn.tree import plot_tree as plot
+import matplotlib.pyplot as plt
 def mostra_arvore(tree, X, y, altura=4):
-    """Plota árvore."""
-    plt.figsize=150
-    tree = TREE(criterion="gini", random_state=42, max_depth=altura)
-    tree.fit(X, y)  # treina no conjunto todo para analisar a árvore
-    fig, ax = plt.subplots(figsize=(14, 6))
-    plot(tree, ax=ax, filled=True)
-    plt.show()
+  plt.figsize=150
+  tree = TREE(criterion="gini", random_state=42, max_depth=altura)
+  tree.fit(X, y)  # treina no conjunto todo para analisar a árvore
+  fig, ax = plt.subplots(figsize=(14, 6))
+  plot(tree, ax=ax, filled=True)
+  plt.show()
